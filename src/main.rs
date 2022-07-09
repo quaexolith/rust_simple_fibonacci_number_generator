295ee9fbf66b8b7cf8fb460910329fe338934ade
quaexolith <quaexolith@gmail.com>
2022-07-09 14:16:27 -0400

    Print Fibonacci numbers 1 through 10

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    for n in 0..10 {
+        print_fibonacci_number(n);
+    }
+}
+
+fn print_fibonacci_number(number: u32) {
+    let index = number + 1;
+
+    println!("Fibonacci number {index}: {}", fibonacci(number));
+}
+
+fn fibonacci(number: u32) -> u32 {
+    match number {
+        0 => 1,
+        1 => 1,
+        _ => fibonacci(number - 1) + fibonacci(number - 2),
+    }
 }
